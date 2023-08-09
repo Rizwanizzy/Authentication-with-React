d0259f89a14a3967688a8f1487541aaee4ea0598
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2023-08-10 01:09:28 +0530

    password reset and password reset confirm has completed

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,6 +137,9 @@ STATICFILES_DIRS=[
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -143,6 +147,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+   'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 DJOSER={
